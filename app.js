0e0d028814572c6c104d13a71a71e3d77ac4a880
ddiaz12 <rdiazmorfin@gmail.com>
2024-01-23 00:55:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
-let numeroMaximo = 1;
+let numeroMaximo = 10;
 
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
